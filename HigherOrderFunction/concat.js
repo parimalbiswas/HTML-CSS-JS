c59bd9553e99a3c9c7eccf205f3e7e826cd49f5d
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-08-29 11:45:56 +0530

    .

---
+++ HigherOrderFunction/concat.js
@@ -0,0 +1,10 @@
+const arr1 = ["Jio", "Khoka", "Mamma"];
+const arr2 = ["Voda", "Ji", "Vagne"];
+const arr3=["Sukuin"]
+
+console.log(arr1.concat(arr2));
+
+console.log(arr1.concat(arr2,arr3));
+
+var complete=[...arr1,...arr2];
+console.log(complete);
---
+++ HigherOrderFunction/join.js
@@ -0,0 +1,12 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruits.join();
+console.log(text);
+
+
+const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+let text2 = fruits2.join(" and ");
+console.log(text2);
+
+// The join() method returns an array as a string.
+
+// The join() method does not change the original array.
